9635c1512ab73e27e6061627369bda496b04ebd1
aketkar18 <anujketkar1118@gmail.com>
2023-01-27 09:07:47 -0600

    Added new project and removed old one

--- functions.js
+++ functions.js
@@ -21,3 +21,12 @@ function change() {
 }
 
 load_style();
+
+function menu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
